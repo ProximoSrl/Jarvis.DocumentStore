6cbf2e00b6eb095aa8380b75d7a9d12e048462fb
Andrea Balducci <mtb.snowboard@gmail.com>
2014-09-26 10:51:50 +0200

    Started rewri

---
+++ src/Jarvis.DocumentStore.Core/Domain/Document/Document.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRS.Kernel.Engine;
+using CQRS.Shared.Events;
+using CQRS.Shared.IdentitySupport;
+
+namespace Jarvis.DocumentStore.Core.Domain.Document
+{
+    public class Document : AggregateRoot<DocumentState>
+    {
+        public void Create(DocumentId id)
+        {
+            RaiseEvent(new DocumentCreated(id));
+        }
+    }
+
+    public class DocumentCreated : DomainEvent
+    {
+        public DocumentCreated(DocumentId id)
+        {
+            this.AggregateId = id;
+        }
+    }
+
+    public class DocumentState : AggregateState
+    {
+        void When(DocumentCreated e)
+        {
+            this.AggregateId = e.AggregateId;
+        }
+    }
+
+    public class DocumentId : EventStoreIdentity
+    {
+        public DocumentId(string id) : base(id)
+        {
+        }
+
+        public DocumentId(long id) : base(id)
+        {
+        }
+    }
+}
---
+++ src/Jarvis.DocumentStore.Tests/DomainSpecs/DocumentSpecifications.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRS.TestHelpers;
+using Jarvis.DocumentStore.Core.Domain.Document;
+using Machine.Specifications;
+
+// ReSharper disable InconsistentNaming
+
+namespace Jarvis.DocumentStore.Tests.DomainSpecs
+{
+    public abstract class DocumentSpecifications : AggregateSpecification<Document, DocumentState>
+    {
+        protected static readonly DocumentId _id = new DocumentId(1);
+        protected static Document Document {
+            get { return Aggregate; }
+        }
+    }
+
+    public class when_a_document_is_created : DocumentSpecifications
+    {
+        Establish context = () => Create();
+
+        Because of = () => Document.Create(_id);
+
+        It DocumentCreatedEvent_should_have_been_raised = () =>
+            EventHasBeenRaised<DocumentCreated>().ShouldBeTrue();
+
+        It DocumentId_should_be_assigned = () =>
+            Document.Id.ShouldBeLike(_id);
+    }
+}
