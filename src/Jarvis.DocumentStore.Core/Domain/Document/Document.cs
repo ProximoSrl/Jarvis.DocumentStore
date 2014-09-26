7fc139134d23f1df43f9a5ed424d10e2255981cc
Andrea Balducci <mtb.snowboard@gmail.com>
2014-09-26 14:15:39 +0200

    Added formats
    updated framework

--- src/Jarvis.DocumentStore.Core/Domain/Document/Document.cs
+++ src/Jarvis.DocumentStore.Core/Domain/Document/Document.cs
@@ -6,14 +6,38 @@ using System.Threading.Tasks;
 using CQRS.Kernel.Engine;
 using CQRS.Shared.Events;
 using CQRS.Shared.IdentitySupport;
+using CQRS.Shared.ValueObjects;
+using Jarvis.DocumentStore.Core.Model;
+using Microsoft.SqlServer.Server;
 
 namespace Jarvis.DocumentStore.Core.Domain.Document
 {
     public class Document : AggregateRoot<DocumentState>
     {
+        public Document(DocumentState initialState)
+            : base(initialState)
+        {
+        }
+
+        public Document()
+        {
+        }
+
         public void Create(DocumentId id)
         {
             RaiseEvent(new DocumentCreated(id));
+        }
+
+        public void AddFormat(FormatId formatId, FileId fileId)
+        {
+            if (InternalState.HasFormat(formatId))
+            {
+                RaiseEvent(new DocumentFormatHasBeenUpdated(formatId, fileId));
+            }
+            else
+            {
+                RaiseEvent(new FormatAddedToDocument(formatId, fileId));
+            }
         }
     }
 
@@ -25,29 +49,83 @@ namespace Jarvis.DocumentStore.Core.Domain.Document
         }
     }
 
+    public class FormatAddedToDocument : DomainEvent
+    {
+        public FormatId FormatId { get; private set; }
+        public FileId FileId { get; private set; }
+
+        public FormatAddedToDocument(FormatId formatId, FileId fileId)
+        {
+            FormatId = formatId;
+            FileId = fileId;
+        }
+    }
+
+    public class DocumentFormatHasBeenUpdated : DomainEvent
+    {
+        public FormatId FormatId { get; private set; }
+        public FileId FileId { get; private set; }
+
+        public DocumentFormatHasBeenUpdated(FormatId formatId, FileId fileId)
+        {
+            FormatId = formatId;
+            FileId = fileId;
+        }
+    }
+
+
     public class DocumentState : AggregateState
     {
+        public IDictionary<FormatId, FileId> Formats { get; private set; }
+
+        public DocumentState(params KeyValuePair<FormatId, FileId>[] formats) : this()
+        {
+            foreach (var keyValuePair in formats)
+            {
+                this.Formats.Add(keyValuePair);
+            }
+        }
+
+        public DocumentState()
+        {
+            this.Formats = new Dictionary<FormatId, FileId>();
+        }
+
         void When(DocumentCreated e)
         {
             this.AggregateId = e.AggregateId;
+        }
+
+        void When(FormatAddedToDocument e)
+        {
+            this.Formats.Add(e.FormatId, e.FileId);
+        }
+
+        public bool HasFormat(FormatId formatId)
+        {
+            return Formats.ContainsKey(formatId);
         }
     }
 
     public class DocumentId : EventStoreIdentity
     {
-        public DocumentId(string id) : base(id)
+        public DocumentId(string id)
+            : base(id)
         {
         }
 
-        public DocumentId(long id) : base(id)
+        public DocumentId(long id)
+            : base(id)
         {
         }
     }
 
-    public class FormatId : StringId
+    public class FormatId : LowercaseStringId
     {
-        public FormatId(string id) : base(id)
+        public FormatId(string id)
+            : base(id)
         {
         }
     }
+
 }
--- src/Jarvis.DocumentStore.Tests/DomainSpecs/DocumentSpecifications.cs
+++ src/Jarvis.DocumentStore.Tests/DomainSpecs/DocumentSpecifications.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CQRS.TestHelpers;
 using Jarvis.DocumentStore.Core.Domain.Document;
+using Jarvis.DocumentStore.Core.Model;
 using Machine.Specifications;
 
 // ReSharper disable InconsistentNaming
@@ -29,16 +30,58 @@ namespace Jarvis.DocumentStore.Tests.DomainSpecs
             EventHasBeenRaised<DocumentCreated>().ShouldBeTrue();
 
         It DocumentId_should_be_assigned = () =>
-            Document.Id.ShouldBeLike(_id);
+            Document.Id.ShouldBeTheSameAs(_id);
     }
 
-    public class when_a_document_is_ : DocumentSpecifications
+    public class when_a_format_is_added_to_a_document : DocumentSpecifications
     {
-        Establish context = () => SetUp(new DocumentState()
-        {
-            
-        });
+        protected static readonly FormatId xmlFormatId = new FormatId("xml");
+        protected static readonly FileId xmlFileId = new FileId("xml");
+        
+        Establish context = () => SetUp(new DocumentState(){});
 
-//        Because of = () => { Document.AddFormat() };
+        Because of = () => Document.AddFormat(xmlFormatId, xmlFileId);
+
+        It FormatAddedToDocument_event_should_have_been_raised = () =>
+            EventHasBeenRaised<FormatAddedToDocument>().ShouldBeTrue();
+
+        It event_should_have_file_and_format_id = () =>
+        {
+            var e = RaisedEvent<FormatAddedToDocument>();
+            e.FileId.ShouldBeTheSameAs(xmlFileId);
+            e.FormatId.ShouldBeTheSameAs(xmlFormatId);
+        };
+
+        It state_should_have_xml_format = () =>
+            State.HasFormat(new FormatId("XML")).ShouldBeTrue();
+    }
+
+    [Subject("document with xml format")]
+    public class document_with_xml_format : DocumentSpecifications
+    {
+        protected static readonly FormatId xmlFormatId1 = new FormatId("xml");
+        protected static readonly FileId xmlFileId1 = new FileId("xml1");
+
+        protected static readonly FormatId xmlFormatId2 = new FormatId("xml");
+        protected static readonly FileId xmlFileId2 = new FileId("xml1");
+
+        public class when_xml_format_is_added : document_with_xml_format
+        {
+            Establish context = () => SetUp(new DocumentState(
+                new KeyValuePair<FormatId,FileId>(xmlFormatId1, xmlFileId1))
+            );
+
+            Because of = () => Document.AddFormat(xmlFormatId2, xmlFileId2);
+
+            It DocumentFormatHasBeenUpdated_event_should_have_been_raised = () =>
+                EventHasBeenRaised<DocumentFormatHasBeenUpdated>().ShouldBeTrue();
+
+            It event_should_have_file_and_format_id = () =>
+            {
+                var e = RaisedEvent<DocumentFormatHasBeenUpdated>();
+                e.FileId.ShouldBeTheSameAs(xmlFileId2);
+                e.FormatId.ShouldBeTheSameAs(xmlFormatId2);
+            };
+        }
     }
 }
