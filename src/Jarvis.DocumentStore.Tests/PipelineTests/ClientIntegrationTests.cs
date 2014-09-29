7440d9ae51509e81b3d896cb346121ad05d61a79
Andrea Balducci <mtb.snowboard@gmail.com>
2014-09-29 16:49:50 +0200

    added multi upload integration test

--- src/Jarvis.DocumentStore.Tests/PipelineTests/ClientIntegrationTests.cs
+++ src/Jarvis.DocumentStore.Tests/PipelineTests/ClientIntegrationTests.cs
@@ -32,14 +32,14 @@ namespace Jarvis.DocumentStore.Tests.PipelineTests
             var client = new DocumentStoreServiceClient(TestConfig.ServerAddress);
 
             Task.WaitAll(
-                //client.Upload(TestConfig.PathToWordDocument, "docx"),
-                //client.Upload(TestConfig.PathToExcelDocument, "xlsx"),
-                //client.Upload(TestConfig.PathToPowerpointDocument, "pptx"),
-                //client.Upload(TestConfig.PathToPowerpointShow, "ppsx"),
-                //client.Upload(TestConfig.PathToOpenDocumentText, "odt"),
-                //client.Upload(TestConfig.PathToOpenDocumentSpreadsheet, "ods"),
-                //client.Upload(TestConfig.PathToOpenDocumentPresentation, "odp"),
-                //client.Upload(TestConfig.PathToRTFDocument, "rtf"),
+                client.Upload(TestConfig.PathToWordDocument, "docx"),
+                client.Upload(TestConfig.PathToExcelDocument, "xlsx"),
+                client.Upload(TestConfig.PathToPowerpointDocument, "pptx"),
+                client.Upload(TestConfig.PathToPowerpointShow, "ppsx"),
+                client.Upload(TestConfig.PathToOpenDocumentText, "odt"),
+                client.Upload(TestConfig.PathToOpenDocumentSpreadsheet, "ods"),
+                client.Upload(TestConfig.PathToOpenDocumentPresentation, "odp"),
+                client.Upload(TestConfig.PathToRTFDocument, "rtf"),
                 client.Upload(TestConfig.PathToHtml, "html")
             );
 
@@ -48,13 +48,30 @@ namespace Jarvis.DocumentStore.Tests.PipelineTests
     }
 
     [TestFixture, Explicit]
-    public class upload_single_file
+    public class upload_to_externa_service
     {
         [Test]
-        public void upload()
+        public void upload_single()
         {
             var client = new DocumentStoreServiceClient(TestConfig.ServerAddress);
             client.Upload(TestConfig.PathToWordDocument, "doc").Wait();
+        }
+
+        [Test]
+        public void upload_multi()
+        {
+            var client = new DocumentStoreServiceClient(TestConfig.ServerAddress);
+            Task.WaitAll(
+                client.Upload(TestConfig.PathToWordDocument, "docx"),
+                client.Upload(TestConfig.PathToExcelDocument, "xlsx"),
+                client.Upload(TestConfig.PathToPowerpointDocument, "pptx"),
+                client.Upload(TestConfig.PathToPowerpointShow, "ppsx"),
+                client.Upload(TestConfig.PathToOpenDocumentText, "odt"),
+                client.Upload(TestConfig.PathToOpenDocumentSpreadsheet, "ods"),
+                client.Upload(TestConfig.PathToOpenDocumentPresentation, "odp"),
+                client.Upload(TestConfig.PathToRTFDocument, "rtf"),
+                client.Upload(TestConfig.PathToHtml, "html")
+            );
         }
     }
 }
