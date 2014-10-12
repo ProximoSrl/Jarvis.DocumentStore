81133fe3e138610ba4b22cff904f639b5c9613be
andreabalducci <mtb.snowboard@gmail.com>
2014-10-12 09:43:16 +0200

    enabled conccurrency retries

--- src/Jarvis.DocumentStore.Core/Jobs/CommandRunnerJob.cs
+++ src/Jarvis.DocumentStore.Core/Jobs/CommandRunnerJob.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using Castle.Core.Logging;
 using CQRS.Kernel.Commands;
+using CQRS.Kernel.Engine;
 using CQRS.Kernel.Store;
 using CQRS.Shared.Commands;
 using Jarvis.DocumentStore.Core.Processing;
@@ -22,7 +24,32 @@ namespace Jarvis.DocumentStore.Core.Jobs
             var cmdString = context.JobDetail.JobDataMap.GetString(JobKeys.Command);
             Logger.DebugFormat("Executing {0}", cmdString);
             var command = CommandSerializer.Deserialize<T>(cmdString);
-            CommandHandler.Handle(command);
+            int i = 0;
+            bool done = false;
+
+            while (!done && i < 100)
+            {
+                try
+                {
+                    CommandHandler.Handle(command);
+                    done = true;
+                }
+                catch (ConflictingCommandException ex)
+                {
+                    // retry
+                    Logger.DebugFormat("Handled {0} {1}, concurrency exception. Retrying",
+                        command.GetType().FullName, command.MessageId);
+                    if (i++ > 5)
+                    {
+                        Thread.Sleep(new Random(DateTime.Now.Millisecond).Next(i * 10));
+                    }
+                }
+                catch (DomainException ex)
+                {
+                    Logger.ErrorFormat(ex, "Failed command {0}", command.MessageId);
+                    done = true;
+                }
+            }
             Logger.DebugFormat("Executed {0}", cmdString);
         }
     }
