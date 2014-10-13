a92bcb34fa1c60460270d6d5d32a245e23f92957
Andrea Balducci <mtb.snowboard@gmail.com>
2014-10-13 13:19:09 +0200

    added skin prerequisites
    Added AngularJS
    
    added ng-app

---
+++ src/Jarvis.DocumentStore.Host/app/app.js
@@ -0,0 +1,5 @@
+﻿(function(window, angular, undefined) {'use strict';
+
+    angular.module('admin', ['admin.layout', 'admin.dashboard']);
+
+})(window, window.angular);
---
+++ src/Jarvis.DocumentStore.Host/app/layout/LayoutController.js
@@ -0,0 +1,15 @@
+﻿(function (window, angular, undefined) {
+    'use strict';
+
+    angular.module('admin.layout').controller('Layout', Layout);
+
+    // Lyaout.$inject = ['']
+
+    function Layout() {
+        var vm = this;
+
+        vm.components = {
+            header: '/layout/header.html'
+        };
+    }
+})(window, window.angular);
