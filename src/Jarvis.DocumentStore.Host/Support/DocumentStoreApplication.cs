7bc4f073fdf72dc14cf1fe04f0960d6c2343e64a
Andrea Balducci <mtb.snowboard@gmail.com>
2014-10-13 09:49:32 +0200

    Added admin libs and start page

--- src/Jarvis.DocumentStore.Host/Support/DocumentStoreApplication.cs
+++ src/Jarvis.DocumentStore.Host/Support/DocumentStoreApplication.cs
@@ -1,6 +1,12 @@
+using System;
+using System.IO;
+using System.Linq;
 using System.Web.Http;
 using CQRS.Shared.Domain;
 using CQRS.Shared.Domain.Serialization;
+using log4net.Repository.Hierarchy;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 using Owin;
 
 namespace Jarvis.DocumentStore.Host.Support
@@ -9,11 +15,52 @@ namespace Jarvis.DocumentStore.Host.Support
     {
         public void Configuration(IAppBuilder application)
         {
+            ConfigureApi(application);
+            ConfigureAdmin(application);
+        }
+
+        void ConfigureAdmin(IAppBuilder application)
+        {
+            var root = AppDomain.CurrentDomain.BaseDirectory
+                .ToLowerInvariant()
+                .Split(Path.DirectorySeparatorChar)
+                .ToList();
+
+            while (true)
+            {
+                var last = root.Last();
+                if (last == String.Empty || last == "debug" || last == "release" || last == "bin")
+                {
+                    root.RemoveAt(root.Count - 1);
+                    continue;
+                }
+
+                break;
+            }
+
+            root.Add("app");
+
+            var appFolder = String.Join(""+Path.DirectorySeparatorChar, root);
+
+            var fileSystem = new PhysicalFileSystem(appFolder);
+
+            var options = new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                FileSystem = fileSystem,
+                EnableDefaultFiles = true
+            };
+
+            application.UseFileServer(options);
+        }
+
+        static void ConfigureApi(IAppBuilder application)
+        {
             var config = new HttpConfiguration
             {
                 DependencyResolver = new WindsorResolver(
                     ContainerAccessor.Instance
-                )
+                    )
             };
 
             config.MapHttpAttributeRoutes();
