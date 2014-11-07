7ce2528bf494880f175102b6d4c5fd9f5a756ba9
andreabalducci <mtb.snowboard@gmail.com>
2014-11-07 07:58:16 +0100

    handling DocumentHandle readmodel promise in a evenstore hook to get che checkpoint id

---
+++ src/Jarvis.DocumentStore.Core/EvenstoreHooks/HandleUpdaterHook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jarvis.DocumentStore.Core.Domain.Document;
+using Jarvis.DocumentStore.Core.Domain.Document.Events;
+using Jarvis.DocumentStore.Core.Domain.Handle;
+using Jarvis.DocumentStore.Core.Domain.Handle.Events;
+using Jarvis.DocumentStore.Core.ReadModel;
+using NEventStore;
+using NEventStore.Persistence;
+
+namespace Jarvis.DocumentStore.Core.EvenstoreHooks
+{
+    public class HandleUpdaterHook : PipelineHookBase
+    {
+        private readonly IHandleWriter _handleWriter;
+        private static readonly string DocumentTypeName = typeof (Document).FullName;
+
+        public HandleUpdaterHook(IHandleWriter handleWriter)
+        {
+            _handleWriter = handleWriter;
+        }
+
+        public override void PostCommit(ICommit committed)
+        {
+            if (!committed.Headers.ContainsKey("AggregateType"))
+                return;
+
+            var type = (string)committed.Headers["AggregateType"];
+            if (type != DocumentTypeName)
+                return;
+
+            var docCreated = committed.Events.Where(x => x.Body is DocumentCreated).Select(x => (DocumentCreated)x.Body).FirstOrDefault();
+            if (docCreated != null)
+            {
+                _handleWriter.Promise(
+                    docCreated.HandleInfo.Handle,
+                    docCreated.HandleInfo.FileName,
+                    (DocumentId)docCreated.AggregateId,
+                    LongCheckpoint.Parse(committed.CheckpointToken).LongValue
+                );
+            }
+        }
+    }
+}
--- src/Jarvis.DocumentStore.Host/Controllers/DocumentsController.cs
+++ src/Jarvis.DocumentStore.Host/Controllers/DocumentsController.cs
@@ -268,7 +268,6 @@ namespace Jarvis.DocumentStore.Host.Controllers
             var handleInfo = new DocumentHandleInfo(handle, fileName, customData);
             var descriptor = _blobStore.GetDescriptor(blobId);
             var createDocument = new CreateDocument(documentId, blobId, handleInfo, descriptor.Hash);
-            _handleWriter.Promise(handle, fileName, documentId, 0);
             CommandBus.Send(createDocument, "api");
         }
 
