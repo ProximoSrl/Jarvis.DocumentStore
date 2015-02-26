f17624d0a41e657f562caaab3a9cf5e47e93c113
Andrea Balducci <mtb.snowboard@gmail.com>
2015-02-26 11:56:09 +0100

    added eventstream api

---
+++ src/Jarvis.DocumentStore.Host/Controllers/EventStreamController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Jarvis.DocumentStore.Host.Model;
+using Jarvis.Framework.Kernel.ProjectionEngine.Client;
+using Jarvis.Framework.Shared.MultitenantSupport;
+using MongoDB.Bson;
+using NEventStore;
+
+namespace Jarvis.DocumentStore.Host.Controllers
+{
+    public class EventStreamController :ApiController, ITenantController
+    {
+        private readonly ICommitEvents _commits;
+        private CommitEnhancer _enhancer;
+        public EventStreamController(IStoreEvents eventStore, CommitEnhancer enhancer)
+        {
+            _enhancer = enhancer;
+            _commits = (ICommitEvents)eventStore;
+        }
+
+        [HttpGet]
+        [Route("{tenantId}/stream/{aggregateId}")]
+        public HttpResponseMessage GetStream(TenantId tenantId, string aggregateId)
+        {
+            if (string.IsNullOrEmpty(aggregateId))
+                return null;
+
+            var commits = _commits.GetFrom("Jarvis", aggregateId,0, int.MaxValue);
+
+            var result = new List<CommitModel>();
+            foreach (var commit in commits)
+            {
+                _enhancer.Enhance(commit);
+                result.Add(new CommitModel(commit));
+            }
+
+            var all = result.ToJson()
+                .Replace("\"_t\"", "\"Type\"")
+                .Replace("ISODate(", "")
+                .Replace("CSUUID(", "")
+                .Replace("\")", "\"")
+            ;
+            
+            var sc = new StringContent(all);
+            sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var resp = new HttpResponseMessage {Content = sc};
+            return resp;
+        }
+    }
+}
---
+++ src/Jarvis.DocumentStore.Host/Model/EventStreamModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NEventStore;
+
+namespace Jarvis.DocumentStore.Host.Model
+{
+    public class CommitModel
+    {
+        public int Sequence { get; private set; }
+        public DateTime Date { get; private set; }
+        public Guid CommitId { get; private set; }
+        public string CheckPoint { get; private set; }
+        
+        public IDictionary<string, object> Headers { get; private set; }
+        public IList<EventMessage> Events { get; private set; }
+
+        public CommitModel(ICommit commit)
+        {
+            this.Sequence = commit.CommitSequence;
+            this.CheckPoint = commit.CheckpointToken;
+            this.Date = commit.CommitStamp;
+            this.CommitId = commit.CommitId;
+            this.Headers = commit.Headers;
+            Events = commit.Events.ToList();
+        }
+    }
+}
