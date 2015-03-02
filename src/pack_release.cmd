2887e0b2cfeccb7652405f79e6fdda61204a0e96
alkampfer <ricci.gianmaria@gmail.com>
2015-03-02 11:21:12 +0100

    changed release package file.

--- src/pack_release.cmd
+++ src/pack_release.cmd
@@ -1,11 +1,21 @@
 rmdir .\publish /s /q
 
-xcopy .\Jarvis.DocumentStore.Host\bin\release .\publish\ /S /Y
-del .\publish\*shost.*
-del .\publish\*.xml
+xcopy .\Jarvis.DocumentStore.Host\bin\release .\publish\Host\ /S /Y
+del .\publish\Host\*shost.*
+del .\publish\Host\*.xml
 rmdir .\publish\logs /s /q
 
+xcopy .\Jarvis.DocumentStore.Host\app .\publish\Host\app\ /S /Y
 
-xcopy .\Jarvis.DocumentStore.Host\app .\publish\app\ /S /Y
+echo Copying jobs
+
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.Attachments\bin\release .\publish\jobs\attachments\ /S /Y
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.Email\bin\release .\publish\jobs\email\ /S /Y
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.HtmlZipOld\bin\release .\publish\jobs\htmlzip\ /S /Y
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.HtmlZip\bin\release .\publish\jobs\htmlzipNew\ /S /Y
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.ImageResizer\bin\release .\publish\jobs\imageresizer\ /S /Y
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.Office\bin\release .\publish\jobs\libreoffice\ /S /Y
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.PdfThumbnail\bin\release .\publish\jobs\pdfthumb\ /S /Y
+xcopy .\Jarvis.DocumentStore.Jobs\Jarvis.DocumentStore.Jobs.Tika\bin\release .\publish\jobs\tika\ /S /Y
 
 pause
