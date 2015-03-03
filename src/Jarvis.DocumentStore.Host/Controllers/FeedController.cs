34795bcec8615702d670f6c3b6838031f15f756e
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2015-03-03 13:17:11 +0100

    Added feed for rm.stream

---
+++ src/Jarvis.DocumentStore.Host/Controllers/FeedController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Jarvis.DocumentStore.Core.ReadModel;
+using Jarvis.DocumentStore.Host.Model;
+using Jarvis.Framework.Shared.MultitenantSupport;
+using Jarvis.Framework.Shared.ReadModel;
+using Newtonsoft.Json;
+
+namespace Jarvis.DocumentStore.Host.Controllers
+{
+    public class FeedController : ApiController, ITenantController
+    {
+        readonly IReader<StreamReadModel, Int64> _streamReadModel;
+
+        public FeedController(IReader<StreamReadModel, long> streamReadModel)
+        {
+            _streamReadModel = streamReadModel;
+        }
+
+        [HttpGet]
+        [Route("{tenantId}/feed/{startId}/{numOfResults}")]
+        public HttpResponseMessage GetFeed(TenantId tenantId, Int64 startId, Int32 numOfResults, [ModelBinder] List<Int32> types)
+        {
+            var baseQuery = _streamReadModel.AllUnsorted
+                .Where(s => s.Id >= startId);
+
+            if (types != null)
+            {
+                var listOfTypes = types.ToList();
+                baseQuery = baseQuery.Where(r => listOfTypes.Contains((Int32) r.EventType));
+            }
+
+            var result = baseQuery.Take(numOfResults)
+                .Select(rm => new FeedModel(rm))
+                .ToList();
+            var sc = new StringContent(JsonConvert.SerializeObject(result));
+            sc.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var resp = new HttpResponseMessage { Content = sc };
+            return resp;
+        }
+    }
+
+
+}
---
+++ src/Jarvis.DocumentStore.Host/Model/FeedModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jarvis.DocumentStore.Core;
+using Jarvis.DocumentStore.Core.ReadModel;
+using Jarvis.DocumentStore.Shared.Model;
+
+namespace Jarvis.DocumentStore.Host.Model
+{
+    public class FeedModel
+    {
+        public String Handle { get; set; }
+
+        public HandleStreamEventTypes EventType { get; set; }
+
+        public String EventTypeDesc { get; set; }
+
+        public String DocumentFormat { get; set; }
+
+        public String FileName { get; set; }
+
+        public String MimeType { get; set; }
+
+
+
+        public FeedModel(StreamReadModel original)
+        {
+            Handle = original.Handle;
+            EventType = original.EventType;
+            EventTypeDesc = original.EventType.ToString();
+            if (original.FormatInfo != null)
+            {
+                DocumentFormat = original.FormatInfo.DocumentFormat;
+            }
+            if (original.Filename != null)
+            {
+                FileName = original.Filename;
+                MimeType = MimeTypes.GetMimeType(original.Filename);
+            }
+            
+            
+        }
+    }
+}
