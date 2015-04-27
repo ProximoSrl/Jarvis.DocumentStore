769b6720b0cd3feba7d5b37e95ba19487c78e3d2
Andrea Balducci <mtb.snowboard@gmail.com>
2015-04-27 17:27:36 +0200

    changed uri

--- src/Jarvis.DocumentStore.Core/BackgroundTasks/ImportFormatFromFileQueue.cs
+++ src/Jarvis.DocumentStore.Core/BackgroundTasks/ImportFormatFromFileQueue.cs
@@ -22,6 +22,8 @@ namespace Jarvis.DocumentStore.Core.BackgroundTasks
         public DocumentFormat Format { get; private set; }
         public TenantId Tenant { get; private set; }
         public DocumentCustomData CustomData { get; private set; }
+
+        public string PathToTaskFile { get; set; }
     }
 
     public class ImportFormatFromFileQueue
@@ -30,11 +32,9 @@ namespace Jarvis.DocumentStore.Core.BackgroundTasks
         public ILogger Logger { get; set; }
 
         private readonly string[] _foldersToWatch;
-        private readonly ConcurrentDictionary<string, FileInQueue> _filesToImport;
         public ImportFormatFromFileQueue(string[] foldersToWatch)
         {
             _foldersToWatch = foldersToWatch;
-            _filesToImport = new ConcurrentDictionary<string,FileInQueue>();
 
         }
 
@@ -43,10 +43,20 @@ namespace Jarvis.DocumentStore.Core.BackgroundTasks
             foreach (var folder in _foldersToWatch)
             {
                 var files = Directory.GetFiles(folder, JobExtension, SearchOption.AllDirectories);
-                foreach (var file in files)
+                Parallel.ForEach(files, file =>
                 {
-                    _filesToImport.GetOrAdd(file.ToLowerInvariant(), LoadTask);
-                }
+                    var task = LoadTask(file);
+                    if (task != null)
+                    {
+                        Logger.DebugFormat("Loading /{0}/{1}/{2} - {3}", 
+                            task.Tenant,
+                            task.Handle,
+                            task.Format,
+                            task.Uri
+                        );
+                    
+                    }
+                });
             }
         }
 
@@ -58,6 +68,7 @@ namespace Jarvis.DocumentStore.Core.BackgroundTasks
                     .Replace("%CURRENT_DIR%", Path.GetDirectoryName(pathToFile).Replace("\\", "/"));
 
                 var task = JsonConvert.DeserializeObject<FileInQueue>(asJson, PocoSerializationSettings.Default);
+                task.PathToTaskFile = pathToFile;
                 return task;
             }
             catch (Exception ex)
