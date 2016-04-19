98636d939a54df98f1a823dce09303550ef8ec5a
alkampfergit <ricci.gianmaria@gmail.com>
2016-04-19 13:02:14 +0200

    Fixed a bug in html generation for emails.
    The generated html contains absolute reference to images (ex c;\temp\blabla") and this makes it not work if you extract the ezip email in another folder.

--- src/Jarvis.DocumentStore.Jobs/Jarvis.DocumentStore.Jobs.Email/MailMessageToHtmlConverterTask.cs
+++ src/Jarvis.DocumentStore.Jobs/Jarvis.DocumentStore.Jobs.Email/MailMessageToHtmlConverterTask.cs
@@ -6,6 +6,8 @@ using Castle.Core.Logging;
 using MsgReader;
 using Path = Jarvis.DocumentStore.Shared.Helpers.DsPath;
 using File = Jarvis.DocumentStore.Shared.Helpers.DsFile;
+using System.Text.RegularExpressions;
+
 namespace Jarvis.DocumentStore.Jobs.Email
 {
     /// <summary>
@@ -26,6 +28,7 @@ namespace Jarvis.DocumentStore.Jobs.Email
             Logger.DebugFormat("Coverting {0} in working folder {1}", pathToEml, workingFolder);
 
             var reader = new Reader();
+
             var outFolder = Path.Combine(workingFolder, jobId);
 
             Logger.DebugFormat("Creating message working folder is {0}", outFolder);
@@ -33,7 +36,9 @@ namespace Jarvis.DocumentStore.Jobs.Email
             Directory.CreateDirectory(outFolder);
 
             Logger.Debug("Extracting files");
+ 
             var files = reader.ExtractToFolder(pathToEml, outFolder);
+
             if (Logger.IsDebugEnabled)
             {
                 foreach (var file in files)
@@ -42,8 +47,19 @@ namespace Jarvis.DocumentStore.Jobs.Email
                 }
                 Logger.DebugFormat("Total files {0}", files.Length);
             }
-            var htmlName = Path.GetFileNameWithoutExtension(files.First(x => x.ToLowerInvariant().EndsWith(".htm")));
-            var pathToZip = Path.Combine(workingFolder, htmlName + ".ezip");
+            var htmlFileName = files.First(x => x.ToLowerInvariant().EndsWith(".htm"));
+            var htmlNameWithoutExtension = Path.GetFileNameWithoutExtension(htmlFileName);
+
+            var htmlContent = File.ReadAllText(htmlFileName);
+            var dirInfoFullName = new DirectoryInfo(outFolder).FullName;
+            htmlContent = Regex.Replace(
+                htmlContent, 
+                @"src=""(?<src>.+?)""", 
+                new MatchEvaluator((m) => NormalizeImgEvaluator(m, dirInfoFullName)), 
+                RegexOptions.IgnoreCase);
+            File.WriteAllText(htmlFileName, htmlContent);
+
+            var pathToZip = Path.Combine(workingFolder, htmlNameWithoutExtension + ".ezip");
 
             Logger.DebugFormat("New zip file is {0}", pathToZip);
 
@@ -64,6 +80,16 @@ namespace Jarvis.DocumentStore.Jobs.Email
                 pathToZip
             );
             return pathToZip;
+        }
+
+        private string NormalizeImgEvaluator(Match match, String directoryPrefix)
+        {
+            var imgGroup = match.Groups["src"].Value.ToLower();
+            if (imgGroup.StartsWith(directoryPrefix.ToLower()))
+            {
+                imgGroup = imgGroup.Substring(directoryPrefix.Length).Trim('/', '\\');
+            }
+            return String.Format("src=\"{0}\"", imgGroup);
         }
     }
 }
