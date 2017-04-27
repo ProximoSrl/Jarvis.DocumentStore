a8ad1f68930d57cdccbf52ea200281117ca4335e
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2017-04-27 11:56:09 +0200

    Fixed Kill Libreoffice Script

--- tools/kill_libreoffice.ps1
+++ tools/kill_libreoffice.ps1
@@ -1,5 +1,13 @@
 $procs = Get-Process soffice -ErrorAction SilentlyContinue
 foreach($proc in $procs) 
 {
+    Write-Output "killing $proc"
+    kill $proc -Force 
+}
+
+$procs = Get-Process "soffice.bin" -ErrorAction SilentlyContinue
+foreach($proc in $procs) 
+{
+    Write-Output "killing $proc"
     kill $proc -Force 
 }
