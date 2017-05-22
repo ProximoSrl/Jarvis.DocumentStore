b27693a4d72480bae961e76f619219464dfdd189
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2017-05-22 15:26:35 +0200

    Admit invalid char in dsimport files.
    Sometimes clients creates .dsimport files with destination
    file that contains invalid char, like \t. With this fix
    all invalid chars are converted to underscore, so the file
    is correctly imported.
    
    Fixed release notes.

---
+++ src/Jarvis.DocumentStore.Shared/Helpers/StringUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarvis.DocumentStore.Shared.Helpers
+{
+    public static class StringUtils
+    {
+        private static Char[] invalidChar = Path.GetInvalidFileNameChars();
+
+        public static string ToSafeFileName(this string fileName, char replaceChar = '_')
+        {
+            var sb = new StringBuilder();
+            foreach (var c in fileName)
+            {
+                if (invalidChar.Contains(c))
+                    sb.Append(replaceChar);
+                else
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public static string ToSafeFileName(this string fileName, Int32 maxFileNameLength)
+        {
+            if (fileName.Length > maxFileNameLength)
+            {
+                fileName = fileName.Substring(0, maxFileNameLength);
+            }
+            return fileName.ToSafeFileName();
+        }
+    }
+}
