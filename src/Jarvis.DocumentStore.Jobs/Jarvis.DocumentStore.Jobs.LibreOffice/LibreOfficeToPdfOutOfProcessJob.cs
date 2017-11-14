3ffe915c5df0897aa932240d1cd76d694c855b4c
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2017-11-14 12:44:31 +0100

    Fixed libreoffice converter with percentage sign.
    We have problem with libreoffice if the name of the
    office file contains a % because it is passed in a url.
    
    Simplified code with % substitution with underscore.

--- src/Jarvis.DocumentStore.Jobs/Jarvis.DocumentStore.Jobs.LibreOffice/LibreOfficeToPdfOutOfProcessJob.cs
+++ src/Jarvis.DocumentStore.Jobs/Jarvis.DocumentStore.Jobs.LibreOffice/LibreOfficeToPdfOutOfProcessJob.cs
@@ -6,43 +6,43 @@ using System.Threading.Tasks;
 
 namespace Jarvis.DocumentStore.Jobs.LibreOffice
 {
-    /// <summary>
-    /// Converts a file to pdf using headless libreoffice
-    /// </summary>
+	/// <summary>
+	/// Converts a file to pdf using headless libreoffice
+	/// </summary>
 
-    public class LibreOfficeToPdfOutOfProcessJob : AbstractOutOfProcessPollerJob
-    {
-        private ILibreOfficeConversion _conversion;
+	public class LibreOfficeToPdfOutOfProcessJob : AbstractOutOfProcessPollerJob
+	{
+		private readonly ILibreOfficeConversion _conversion;
 
-        public LibreOfficeToPdfOutOfProcessJob(ILibreOfficeConversion conversion)
-        {
-            base.PipelineId = "office";
-            base.QueueName = "office";
-            _conversion = conversion;
-            _conversion.Initialize();
-        }
+		public LibreOfficeToPdfOutOfProcessJob(ILibreOfficeConversion conversion)
+		{
+			base.PipelineId = "office";
+			base.QueueName = "office";
+			_conversion = conversion;
+			_conversion.Initialize();
+		}
 
-        protected async override Task<ProcessResult> OnPolling(PollerJobParameters parameters, string workingFolder)
-        {
-            Logger.DebugFormat(
-               "Delegating conversion of file {0} to libreoffice",
-               parameters.JobId
-           );
+		protected async override Task<ProcessResult> OnPolling(PollerJobParameters parameters, string workingFolder)
+		{
+			Logger.DebugFormat(
+			   "Delegating conversion of file {0} to libreoffice",
+			   parameters.JobId
+		   );
 
-            //libreofficeconversion is registered per tenant.
+			//libreofficeconversion is registered per tenant.
 
-            string pathToFile = await DownloadBlob(parameters.TenantId, parameters.JobId, parameters.FileName, workingFolder);
-            
-            Logger.DebugFormat("Downloaded file {0} to be converted to pdf", pathToFile);
-            var outputFile = _conversion.Run(pathToFile, "pdf");
+			string pathToFile = await DownloadBlob(parameters.TenantId, parameters.JobId, parameters.FileName, workingFolder);
 
-            await AddFormatToDocumentFromFile(
-                parameters.TenantId,
-                parameters.JobId,
-                new DocumentFormat(DocumentFormats.Pdf),
-                outputFile,
-                new Dictionary<string, object>());
-            return ProcessResult.Ok;
-        }
-    }
+			Logger.DebugFormat("Downloaded file {0} to be converted to pdf", pathToFile);
+			var outputFile = _conversion.Run(pathToFile, "pdf");
+
+			await AddFormatToDocumentFromFile(
+				parameters.TenantId,
+				parameters.JobId,
+				new DocumentFormat(DocumentFormats.Pdf),
+				outputFile,
+				new Dictionary<string, object>()).ConfigureAwait(false);
+			return ProcessResult.Ok;
+		}
+	}
 }
--- src/Jarvis.DocumentStore.Jobs/Jarvis.DocumentStore.Jobs.LibreOffice/LibreOfficeUnoConversion.cs
+++ src/Jarvis.DocumentStore.Jobs/Jarvis.DocumentStore.Jobs.LibreOffice/LibreOfficeUnoConversion.cs
@@ -13,201 +13,216 @@ using Path = Jarvis.DocumentStore.Shared.Helpers.DsPath;
 using File = Jarvis.DocumentStore.Shared.Helpers.DsFile;
 namespace Jarvis.DocumentStore.Jobs.LibreOffice
 {
-    /// <summary>
-    /// http://tinyway.wordpress.com/2011/03/30/how-to-convert-office-documents-to-pdf-using-open-office-in-c/
-    /// </summary>
-    public class LibreOfficeUnoConversion : ILibreOfficeConversion
-    {
-        static readonly object LockRoot = new object();
+	/// <summary>
+	/// http://tinyway.wordpress.com/2011/03/30/how-to-convert-office-documents-to-pdf-using-open-office-in-c/
+	/// </summary>
+	public class LibreOfficeUnoConversion : ILibreOfficeConversion
+	{
+		static readonly object LockRoot = new object();
 
-        public ILogger Logger { get; set; }
+		public ILogger Logger { get; set; }
 
-        readonly JobsHostConfiguration _config;
+		readonly JobsHostConfiguration _config;
 
-        public LibreOfficeUnoConversion(JobsHostConfiguration config)
-        {
-            //Needed by UNO SDK5. 
-            //http://stackoverflow.com/questions/31856025/bootstrap-uno-api-libreoffice-exception
-            //look at comments of funbit. We need to set UNO_PATH and soffice should be in the PATH
-            //of the system.
-            var sofficePath = config.GetPathToLibreOffice();
-            var unoPath = Path.GetDirectoryName(sofficePath);
-            Environment.SetEnvironmentVariable("UNO_PATH", unoPath, EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH") + @";" + unoPath, EnvironmentVariableTarget.Process);
+		public LibreOfficeUnoConversion(JobsHostConfiguration config)
+		{
+			//Needed by UNO SDK5. 
+			//http://stackoverflow.com/questions/31856025/bootstrap-uno-api-libreoffice-exception
+			//look at comments of funbit. We need to set UNO_PATH and soffice should be in the PATH
+			//of the system.
+			var sofficePath = config.GetPathToLibreOffice();
+			var unoPath = Path.GetDirectoryName(sofficePath);
+			Environment.SetEnvironmentVariable("UNO_PATH", unoPath, EnvironmentVariableTarget.Process);
+			Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH") + @";" + unoPath, EnvironmentVariableTarget.Process);
 
-            _config = config;
-            Logger = NullLogger.Instance;
-        }
+			_config = config;
+			Logger = NullLogger.Instance;
+		}
 
-        public string Run(string sourceFile, string outType)
-        {
-            var outputFile = Path.ChangeExtension(sourceFile, outType);
+		public string Run(string sourceFile, string outType)
+		{
+			var outputFile = Path.ChangeExtension(sourceFile, outType);
 
-            Logger.DebugFormat("UNO CONVERSION: Converting: {0} to {1}", sourceFile, outputFile);
+			Logger.DebugFormat("UNO CONVERSION: Converting: {0} to {1}", sourceFile, outputFile);
 
-            lock (LockRoot) // -> single runner (todo: more user profiles)
-            {
-                ConvertToPdf(sourceFile, outputFile);
-            }
+			lock (LockRoot) // -> single runner (todo: more user profiles)
+			{
+				ConvertToPdf(sourceFile, outputFile);
+			}
 
-            if (!File.Exists(outputFile))
-                throw new Exception("Conversion failed");
+			if (!File.Exists(outputFile))
+				throw new Exception("Conversion failed");
 
+			return outputFile;
+		}
 
-            return outputFile;
-        }
+		public void Initialize()
+		{
+			CloseOpenOffice();
+		}
 
-        public void Initialize()
-        {
-            CloseOpenOffice();
-        }
+		public void ConvertToPdf(string inputFile, string outputFile)
+		{
+			inputFile = SanitizeFileName(inputFile);
+			if (ConvertExtensionToFilterType(Path.GetExtension(inputFile)) == null)
+				throw new InvalidProgramException("Unknown file type for OpenOffice. File = " + inputFile);
 
-        public void ConvertToPdf(string inputFile, string outputFile)
-        {
-            if (ConvertExtensionToFilterType(Path.GetExtension(inputFile)) == null)
-                throw new InvalidProgramException("Unknown file type for OpenOffice. File = " + inputFile);
+			StartOpenOffice();
 
-            StartOpenOffice();
+			//Get a ComponentContext
+			var xLocalContext = Bootstrap.bootstrap();
+			//Get MultiServiceFactory
+			var xRemoteFactory = (XMultiServiceFactory)xLocalContext.getServiceManager();
+			//Get a CompontLoader
+			var aLoader = (XComponentLoader)xRemoteFactory.createInstance("com.sun.star.frame.Desktop");
+			//Load the sourcefile
 
-            //Get a ComponentContext
-            var xLocalContext = Bootstrap.bootstrap();
-            //Get MultiServiceFactory
-            var xRemoteFactory = (XMultiServiceFactory)xLocalContext.getServiceManager();
-            //Get a CompontLoader
-            var aLoader = (XComponentLoader)xRemoteFactory.createInstance("com.sun.star.frame.Desktop");
-            //Load the sourcefile
+			XComponent xComponent = null;
+			try
+			{
+				xComponent = InitDocument(aLoader, PathConverter(inputFile), "_blank");
+				//Wait for loading
+				while (xComponent == null)
+				{
+					Thread.Sleep(1000);
+				}
 
-            XComponent xComponent = null;
-            try
-            {
-                xComponent = InitDocument(aLoader, PathConverter(inputFile), "_blank");
-                //Wait for loading
-                while (xComponent == null)
-                {
-                    Thread.Sleep(1000);
-                }
+				// save/export the document
+				SaveDocument(xComponent, inputFile, PathConverter(outputFile));
+			}
+			catch (Exception ex)
+			{
+				Logger.Error($"Error during conversion of file {inputFile}", ex);
+				throw;
+			}
+			finally
+			{
+				if (xComponent != null) xComponent.dispose();
+			}
+		}
 
-                // save/export the document
-                SaveDocument(xComponent, inputFile, PathConverter(outputFile));
-            }
-            finally
-            {
-                if (xComponent != null) xComponent.dispose();
-            }
-        }
+		private static string SanitizeFileName(string inputFile)
+		{
+			if (inputFile.Contains("%"))
+			{
+				//percentage sign create trouble
+				var sanitizeFileName = inputFile.Replace("%", "_");
+				File.Move(inputFile, sanitizeFileName);
+				inputFile = sanitizeFileName;
+			}
 
-        public void CloseOpenOffice()
-        {
-            var ps = Process.GetProcessesByName("soffice.bin");
-            foreach (var process in ps)
-            {
-                Logger.DebugFormat("Closing openoffice pid: {0}", process.Id);
-                process.Kill();
-            }
+			return inputFile;
+		}
 
-            ps = Process.GetProcessesByName(_config.GetPathToLibreOffice());
-            foreach (var process in ps)
-            {
-                Logger.DebugFormat("Closing openoffice pid: {0}", process.Id);
-                process.Kill();
-            }
-        }
+		public void CloseOpenOffice()
+		{
+			var ps = Process.GetProcessesByName("soffice.bin");
+			foreach (var process in ps)
+			{
+				Logger.DebugFormat("Closing openoffice pid: {0}", process.Id);
+				process.Kill();
+			}
 
-        private void StartOpenOffice()
-        {
-            var ps = Process.GetProcessesByName("soffice");
-            if (ps.Length > 0)
-                return;
+			ps = Process.GetProcessesByName(_config.GetPathToLibreOffice());
+			foreach (var process in ps)
+			{
+				Logger.DebugFormat("Closing openoffice pid: {0}", process.Id);
+				process.Kill();
+			}
+		}
 
-            var pathToLibreOffice = _config.GetPathToLibreOffice();
-            var p = new Process
-            {
-                StartInfo =
-                {
-                    Arguments = "-headless -nofirststartwizard",
-                    FileName = pathToLibreOffice,
-                    CreateNoWindow = true
-                }
-            };
+		private void StartOpenOffice()
+		{
+			var ps = Process.GetProcessesByName("soffice");
+			if (ps.Length > 0)
+				return;
 
-            Logger.DebugFormat(
-                "Starting liberoffice headless {0} {1}",
-                p.StartInfo.FileName,
-                p.StartInfo.Arguments
-            );
+			var pathToLibreOffice = _config.GetPathToLibreOffice();
+			var p = new Process
+			{
+				StartInfo =
+				{
+					Arguments = "-headless -nofirststartwizard",
+					FileName = pathToLibreOffice,
+					CreateNoWindow = true
+				}
+			};
 
-            var result = p.Start();
+			Logger.DebugFormat(
+				"Starting liberoffice headless {0} {1}",
+				p.StartInfo.FileName,
+				p.StartInfo.Arguments
+			);
 
-            if (result == false)
-                throw new InvalidProgramException("OpenOffice failed to start.");
-        }
+			var result = p.Start();
 
-        private XComponent InitDocument(XComponentLoader aLoader, string file, string target)
-        {
-            var openProps = new PropertyValue[1];
-            openProps[0] = new PropertyValue { Name = "Hidden", Value = new Any(true) };
+			if (result == false)
+				throw new InvalidProgramException("OpenOffice failed to start.");
+		}
 
-            var xComponent = aLoader.loadComponentFromURL(
-                file, target, 0,
-                openProps);
+		private XComponent InitDocument(XComponentLoader aLoader, string file, string target)
+		{
+			var openProps = new PropertyValue[1];
+			openProps[0] = new PropertyValue { Name = "Hidden", Value = new Any(true) };
 
-            return xComponent;
-        }
+			var xComponent = aLoader.loadComponentFromURL(
+				file, target, 0,
+				openProps);
 
-        private void SaveDocument(XComponent xComponent, string sourceFile, string destinationFile)
-        {
-            var propertyValues = new PropertyValue[2];
-            // Setting the flag for overwriting
-            propertyValues[1] = new PropertyValue { Name = "Overwrite", Value = new Any(true) };
-            //// Setting the filter name
-            propertyValues[0] = new PropertyValue
-            {
-                Name = "FilterName",
-                Value = new Any(ConvertExtensionToFilterType(Path.GetExtension(sourceFile)))
-            };
-            ((XStorable)xComponent).storeToURL(destinationFile, propertyValues);
-        }
+			return xComponent;
+		}
 
-        private string PathConverter(string file)
-        {
-            if (string.IsNullOrEmpty(file))
-                throw new NullReferenceException("Null or empty path passed to OpenOffice");
+		private void SaveDocument(XComponent xComponent, string sourceFile, string destinationFile)
+		{
+			var propertyValues = new PropertyValue[2];
+			// Setting the flag for overwriting
+			propertyValues[1] = new PropertyValue { Name = "Overwrite", Value = new Any(true) };
+			//// Setting the filter name
+			propertyValues[0] = new PropertyValue
+			{
+				Name = "FilterName",
+				Value = new Any(ConvertExtensionToFilterType(Path.GetExtension(sourceFile)))
+			};
+			((XStorable)xComponent).storeToURL(destinationFile, propertyValues);
+		}
 
-            return String.Format("file:///{0}", file.Replace(@"\", "/"));
-        }
+		private string PathConverter(string file)
+		{
+			if (string.IsNullOrEmpty(file))
+				throw new ArgumentNullException("Null or empty path passed to OpenOffice");
 
-        public string ConvertExtensionToFilterType(string extension)
-        {
-            switch (extension)
-            {
-                case ".doc":
-                case ".docx":
-                case ".txt":
-                case ".rtf":
-                case ".html":
-                case ".htm":
-                case ".xml":
-                case ".odt":
-                case ".wps":
-                case ".wpd":
-                    return "writer_pdf_Export";
-                case ".xls":
-                case ".xlsb":
-                case ".xlsx":
-                case ".ods":
-                    return "calc_pdf_Export";
-                case ".ppt":
-                case ".ppsx":
-                case ".pptx":
-                case ".odp":
-                    return "impress_pdf_Export";
+			return String.Format("file:///{0}", file.Replace(@"\", "/"));
+		}
 
-                default:
-                    return null;
-            }
-        }
+		public string ConvertExtensionToFilterType(string extension)
+		{
+			switch (extension)
+			{
+				case ".doc":
+				case ".docx":
+				case ".txt":
+				case ".rtf":
+				case ".html":
+				case ".htm":
+				case ".xml":
+				case ".odt":
+				case ".wps":
+				case ".wpd":
+					return "writer_pdf_Export";
+				case ".xls":
+				case ".xlsb":
+				case ".xlsx":
+				case ".ods":
+					return "calc_pdf_Export";
+				case ".ppt":
+				case ".ppsx":
+				case ".pptx":
+				case ".odp":
+					return "impress_pdf_Export";
 
-
-
-    }
+				default:
+					return null;
+			}
+		}
+	}
 }
